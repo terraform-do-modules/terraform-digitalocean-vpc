cd7ce424b0ee93af0b845c02c098d767b9a7027e
anmolnagpal <ianmolnagpal@gmail.com>
2021-08-09 19:24:06 +0530

    update

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ locals {
 #Module      : LABEL
 #Description : Terraform label module variables.
 module "labels" {
-  source = "git::https://github.com/terraform-do-modules/terraform-digitalocean-labels.git?ref=0.15"
+  source      = "git::https://github.com/terraform-do-modules/terraform-digitalocean-labels.git?ref=0.15"
   name        = var.name
   environment = var.environment
   label_order = var.label_order
