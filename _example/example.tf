357e63f33e995147ab21b406d98a956cfe68c487
mahesh yadav <devopsmahesh.it@gmail.com>
2023-05-30 19:26:50 +0530

    fix: remove token

--- _example/example.tf
+++ _example/example.tf
@@ -2,7 +2,7 @@
 provider "digitalocean" {
   # You need to set this in your .bashrc
   # export DIGITALOCEAN_TOKEN="Your API TOKEN"
-  token = "dop_v1_677763b63d63a9cba824df50b797d03a080e1987de4c014682df19c080fd1dad"
+  token = ""
 }
 
 module "vpc" {
