ad48c6cc85ebcf83dbe1e3f830e7b406bc1e0f8e
CloudDrove CI <84795582+clouddrove-ci@users.noreply.github.com>
2026-03-20 14:44:25 +0100

    chore: add missing basic/complete examples (#83)
    Co-authored-by: Anmol Nagpal <anmolnagpal@users.noreply.github.com>

---
+++ _examples/basic/example.tf
@@ -0,0 +1,5 @@
+provider "digitalocean" {}
+
+module "vpc" {
+  source = "../../"
+}
---
+++ _examples/basic/versions.tf
@@ -0,0 +1,10 @@
+# Terraform / OpenTofu version
+terraform {
+  required_version = ">= 1.10.0"
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = ">= 2.70.0"
+    }
+  }
+}
