0cb3ad3f0d0721814bc7553da45111d7fc9c4868
CloudDrove CI <84795582+clouddrove-ci@users.noreply.github.com>
2026-05-10 03:58:11 +0200

    fix: clean output description typos (Foundry: FOU-203) (#97)

--- outputs.tf
+++ outputs.tf
@@ -2,17 +2,17 @@
 #Description : VPCs are virtual networks containing resources that can communicate with each other in full isolation, using private IP addresses.
 output "id" {
   value       = digitalocean_vpc.default[0].id
-  description = "The unique identifier for the VPC.."
+  description = "The unique identifier for the VPC."
 }
 
 output "urn" {
   value       = digitalocean_vpc.default[0].urn
-  description = " The uniform resource name (URN) for the VPC."
+  description = "The uniform resource name (URN) for the VPC."
 }
 
 output "default" {
   value       = digitalocean_vpc.default[0].default
-  description = " A boolean indicating whether or not the VPC is the default one for the region."
+  description = "A boolean indicating whether or not the VPC is the default one for the region."
 }
 
 output "created_at" {
